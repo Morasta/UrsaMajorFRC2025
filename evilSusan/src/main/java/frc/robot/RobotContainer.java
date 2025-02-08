4600a5af0f7c1afc31d8ba874e9ce83d0e60c1e7
RenWraps <renmartin8800@gmail.com>
2025-02-08 12:17:19 -0800

    fixed the joystick controls for driving

--- evilSusan/src/main/java/frc/robot/RobotContainer.java
+++ evilSusan/src/main/java/frc/robot/RobotContainer.java
@@ -57,9 +57,9 @@ public class RobotContainer {
 
         m_robotDrive.setDefaultCommand(
             new RunCommand(() -> m_robotDrive.drive(
-                -m_driverController.getRawAxis(0),
-                -m_driverController.getRawAxis(4),
-                -m_driverController.getRawAxis(2),
+                -m_driverController.getRawAxis(1),
+                -m_driverController.getRawAxis(5),
+                -m_driverController.getRawAxis(3),
                 true), m_robotDrive
             )
         );
