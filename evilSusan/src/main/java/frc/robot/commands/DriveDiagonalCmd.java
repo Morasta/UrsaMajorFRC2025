5226256cf34f150dc785b52326e6a57c9d6a7745
Ricky Broccoli <glover7@gresham.k12.or.us>
2025-02-11 17:39:44 -0800

    Update DriveDiagonalCmd.java

--- evilSusan/src/main/java/frc/robot/commands/DriveDiagonalCmd.java
+++ evilSusan/src/main/java/frc/robot/commands/DriveDiagonalCmd.java
@@ -30,12 +30,13 @@ public class DriveDiagonalCmd extends Command {
     public void execute() {
         printStatus("executed");
         driveSubsystem.setMotors(AutoConstants.kAutoDriveDiagonalSpeed, AutoConstants.kAutoDriveDiagonalSpeed);
+        driveSubsystem.setMotors(1, 0, 0, 1);
     }
 
     @Override
     public void end(boolean interrupted) {
         printStatus("end");
-        //driveSubsystem.setMotors(1, 0, 0, 1);
+        driveSubsystem.setMotors(1, 0, 0, 1);
         System.out.println(this.getClass().getSimpleName() + " executed");
     }
 }
