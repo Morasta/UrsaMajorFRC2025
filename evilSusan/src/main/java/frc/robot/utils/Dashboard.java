656f04ad3b0d64e0803b955c8113a6a03982dfc4
Ricky Broccoli <glover7@gresham.k12.or.us>
2025-02-19 17:20:14 -0800

    Dashboard.java

--- evilSusan/src/main/java/frc/robot/utils/Dashboard.java
+++ evilSusan/src/main/java/frc/robot/utils/Dashboard.java
@@ -1,6 +1,5 @@
 public class test {
-  public T void addToDashboard(String tab, String text, T value) {
-    /* do the work here. this is an example code. */
-    
+  public <T> void addToDashboard(String tab, String text, T value) {
+    /* do the work here. this is an example code with a special generic type. */
   }
 }
