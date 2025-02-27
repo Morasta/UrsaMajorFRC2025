2bb5d0973d1a74e5d56c873c6ec3fdfbff86cf24
Matt Viehdorfer <Morasta@users.noreply.github.com>
2025-02-26 17:35:17 -0800

    Update DepositCoralCmd.java

--- evilSusan/src/main/java/frc/robot/commands/groups/DepositCoralCmd.java
+++ evilSusan/src/main/java/frc/robot/commands/groups/DepositCoralCmd.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.groups;
 
 import edu.wpi.first.wpilibj2.command.CommandGroup;
+
 import frc.robot.commands.elevator.ElevatorSlideCmd;
 import frc.robot.commands.elevator.ElevatorVerticalCmd;
 import frc.robot.commands.intake.IntakeSetOpenCmd;
@@ -8,11 +9,9 @@ import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class DepositCoralCommandGroup extends CommandGroup {
-
     public DepositCoralCmd(ElevatorSubsystem elevatorSubsystem, IntakeSubsystem intakeSubsystem) {
-        addSequential (new ElevatorVerticalCmd(elevatorSubsystem, double 1.0));
-        addSequential (new ElevatorSlideCmd(elevatorSubsystem, double 1.0));
+        addSequential (new ElevatorVerticalCmd(elevatorSubsystem, 1.0));
+        addSequential (new ElevatorSlideCmd(elevatorSubsystem, 1.0));
         addSequential (new IntakeSetOpenCmd(intakeSubsystem, true));
     }
-
 }
