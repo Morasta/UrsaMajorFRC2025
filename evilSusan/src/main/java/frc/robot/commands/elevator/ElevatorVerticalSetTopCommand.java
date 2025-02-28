8bc7326503d1dfe67e173738cd671c27eae89302
Ricky Broccoli <glover7@gresham.k12.or.us>
2025-02-27 16:46:51 -0800

    Create ElevatorVerticalSetTopCommand.java

---
+++ evilSusan/src/main/java/frc/robot/commands/elevator/ElevatorVerticalSetTopCommand.java
@@ -0,0 +1,47 @@
+package frc.robot.commands.elevator;
+
+//import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ElevatorSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants.ElevatorVerticalPositions;
+
+
+public class ElevatorVerticalCmd extends Command{
+    private final ElevatorSubsystem elevatorSubsystem;
+    private final double speed;
+
+    public ElevatorVerticalCmd(ElevatorSubsystem elevatorSubsystem, double speed) {
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.speed = speed;
+        addRequirements(elevatorSubsystem);
+    }
+
+    public ElevatorVerticalCmd(ElevatorSubsystem elevatorSubsystem, ElevatorVerticalPositions targetPosition, double speed) {
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.speed = speed;
+        addRequirements(elevatorSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("ElevatorJoystickCmd started!");
+    }
+
+    @Override
+    public void execute() {
+        elevatorSubsystem.setSlideMotor(speed);
+        System.out.println("executing elevator vertical command, " + speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevatorSubsystem.setVerticalMotor(0);
+        System.out.println("ElevatorJoystickCmd ended!");
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
