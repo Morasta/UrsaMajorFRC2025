ee0498d1f0c8bdc36fa216a4916b159a1404f05a
Ricky Broccoli <glover7@gresham.k12.or.us>
2025-02-27 16:30:55 -0800

    Update Constants.java

--- evilSusan/src/main/java/frc/robot/Constants.java
+++ evilSusan/src/main/java/frc/robot/Constants.java
@@ -18,6 +18,9 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public final class Constants {
     // Constant definitions for controller mapping used by the "operator" or driver
     // This should map to the controller classes we'll define later
+
+    double GRAVITY_COMPENSATION = 1.0;
+    
     public static final class OIConstants {
         //Joystick ports
         public static final int kDriverJoystickPort = 0;
