54bceaf13303565a826b209516efa12f8c15272d
Ricky Broccoli <glover7@gresham.k12.or.us>
2025-03-20 16:32:27 -0700

    remove todos(breaks literally everything(in a fiery code explosion))

--- evilSusan/src/main/java/frc/robot/commands/drive/DriveLeftDiagonalCmd.java
+++ evilSusan/src/main/java/frc/robot/commands/drive/DriveLeftDiagonalCmd.java
@@ -20,7 +20,7 @@ public class DriveLeftDiagonalCmd extends Command {
     public DriveLeftDiagonalCmd(DriveTrain driveTrain, double distance, double speed) {
         printStatus("Created");
         this.driveSubsystem = driveTrain;
-        this.distance = 1; //TODO: Fix me
+        this.distance = 1; 
         //* this.distance = DriveTrain.getEncoderMeters() + distance; */
         addRequirements(driveSubsystem);
     }
