2f129fa48b4c04d7f8d81bf3b541ec5078da2458
Ricky Broccoli <glover7@gresham.k12.or.us>
2025-03-20 16:29:36 -0700

    Update DriveBackwardCmd.java

--- evilSusan/src/main/java/frc/robot/commands/drive/DriveBackwardCmd.java
+++ evilSusan/src/main/java/frc/robot/commands/drive/DriveBackwardCmd.java
@@ -18,7 +18,7 @@ public class DriveBackwardCmd extends Command {
     public DriveBackwardCmd(DriveTrain driveTrain, double distance, double speed) {
         printStatus("Created");
         this.driveSubsystem = driveTrain;
-        this.distance = 1; //TODO: Fix me
+        this.distance = 1;
         //* this.distance = DriveTrain.getEncoderMeters() + distance; */
         addRequirements(driveSubsystem);
     }
